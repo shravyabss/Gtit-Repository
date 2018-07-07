304accbcd2a401f77df4262b4f3652c208b72fae
shravyabss <shravyab@seguro-soft.com>
2018-07-07 16:19:29 +0530

    Changed name to width-123

--- GitRepositoryApplication/GitRepositoryApplication/App_Start/Newfile.js
+++
@@ -1,5 +0,0 @@
-﻿@Scripts.Render("~/bundles/responds.js")
-@Scripts.Render("~/bundles/jquery-1.11.3.min.js")
-@Scripts.Render("~/bundles/jssor.slider-22.0.15.mini.js")
-<script type="text/javascript">
-</script>
