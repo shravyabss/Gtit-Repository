f32f954fc58a5124da744c5464025c8269bb4344
shravyabss <shravyab@seguro-soft.com>
2018-07-07 14:49:06 +0530

    New commit for js file in childbranch

--- GitRepositoryApplication/GitRepositoryApplication/App_Start/Newfile.js
+++ GitRepositoryApplication/GitRepositoryApplication/App_Start/Newfile.js
@@ -1,4 +1,5 @@
-﻿@Scripts.Render("~/bundles/responds.js")
+﻿<script>
+@Scripts.Render("~/bundles/responds.js")
 @Scripts.Render("~/bundles/jquery-1.11.3.min.js")
 @Scripts.Render("~/bundles/jssor.slider-22.0.15.mini.js")
 <script type="text/javascript">
